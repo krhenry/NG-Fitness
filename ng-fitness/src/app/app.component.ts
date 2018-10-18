fdec9441c910d325e70ec4e86d833996858af879
Kyle Henry <krhenry433@gmail.com>
2018-10-18 12:51:06 -0400

    Added login form. Added nav bar.

--- ng-fitness/src/app/app.component.ts
+++ ng-fitness/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-fitness';
+  // title = 'ng-fitness';
+  // openSidenav = false;
+
+  // @ViewChild('sidenav')
+  // onToggle() {
+
+  // }
 }
